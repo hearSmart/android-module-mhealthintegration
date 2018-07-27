80664b2c03edca36f2e1fabc880802d73863d8dc
David Howe <umbredave@gmail.com>
2018-07-27 18:24:15 +0200

    - NEW Sample library

---
+++ sample/app/src/main/java/com/hearxgroup/integrationtest/MainActivity.java
@@ -0,0 +1,103 @@
+package com.hearxgroup.integrationtest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.hearxgroup.mhealthintegration.Constants;
+import com.hearxgroup.mhealthintegration.Contracts.MHealthTestRetrieverContract;
+import com.hearxgroup.mhealthintegration.Models.HearscreenTest;
+import com.hearxgroup.mhealthintegration.Models.MHealthTestRequest;
+import com.hearxgroup.mhealthintegration.TestRequestHelper;
+import com.hearxgroup.mhealthintegration.Util;
+
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity implements MHealthTestRetrieverContract.TestRetrieverInterface {
+
+    private final String TAG = getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String testId = getRandomSequence();
+                MHealthTestRequest testRequest =
+                        MHealthTestRequest.build(
+                                testId,
+                                "male",
+                                "1992-02-21",
+                                "John",
+                                "Smith",
+                                "eng",
+                                null,
+                                null,
+                                null,
+                                null,
+                                "com.hearxgroup.mhealthintegrationdemo.mhealthtest");
+
+                String requestValidationResponse = Util.validateTestRequest(MainActivity.this, testRequest);
+                if(requestValidationResponse==null)
+                    TestRequestHelper.startTest(MainActivity.this, testRequest);
+                else
+                    Log.e("MainActivity", "Validation error:"+requestValidationResponse);
+            }
+        });
+
+        onNewIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleLaunchIntent(intent);
+        setIntent(null);
+    }
+
+    private void handleLaunchIntent(Intent intent) {
+        String mHealthTestId = TestRequestHelper.getTestIdFromIntent(intent);
+        if(mHealthTestId!=null) {
+            //RETURN FROM A TEST REQUEST OCCURRED
+            Log.d(TAG, "RETURN FROM AN MHEALTH TEST REQUEST OCCURRED");
+            TestRequestHelper.retrieveTestResult(
+                    MainActivity.this,
+                    getLoaderManager(),
+                    MainActivity.this,
+                    Constants.INDEX_HEARSCREEN,
+                    mHealthTestId);
+        }
+    }
+
+    public static String getRandomSequence() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+
+    @Override
+    public void onRetrieveTestHearScreen(HearscreenTest hearscreenTest) {
+        Log.d(TAG, "onRetrieveTestHearScreen");
+        Log.d(TAG, "hearscreenTest:"+hearscreenTest.toJson());
+    }
+
+    @Override
+    public void onRetrieveTestHearTest(HearscreenTest hearscreenTest) {
+        Log.d(TAG, "onRetrieveTestHearTest");
+        Log.d(TAG, "hearscreenTest:"+hearscreenTest.toJson());
+    }
+
+    @Override
+    public void onRetrieveTestPeekAcuity(HearscreenTest hearscreenTest) {
+        Log.d(TAG, "onRetrieveTestPeekAcuity");
+        Log.d(TAG, "hearscreenTest:"+hearscreenTest.toJson());
+    }
+
+    @Override
+    public void onRetrieveTestError(String errorMessage) {
+        Log.e(TAG, "onRetrieveTestError: "+errorMessage);
+    }
+}
